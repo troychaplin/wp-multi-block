0069f8b1d4765d4117dbac688b2686c58d6e0249
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-30 17:58:45 -0400

    added editor and frontend enqueues

--- wp-multi-block.php
+++ wp-multi-block.php
@@ -35,3 +35,35 @@ function multiblock_register_blocks()
 	}
 }
 add_action('init', 'multiblock_register_blocks');
+
+/**
+ * Enqueues the block's assets for the editor.
+ */
+function multiblock_enqueue_block_assets()
+{
+	wp_enqueue_script(
+		'multi-block-editor-js',
+		plugin_dir_url(__FILE__) . 'build/multi-block-editor.js',
+		['wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'],
+		null,
+		false
+	);
+
+	wp_enqueue_style(
+		'multi-block-editor-css',
+		plugin_dir_url(__FILE__) . 'build/multi-block-editor.css',
+		[],
+		null
+	);
+}
+add_action('enqueue_block_editor_assets', 'multiblock_enqueue_block_assets');
+
+/**
+ * Enqueues the frontend assets for the blocks.
+ */
+function multiblock_enqueue_frontend_assets()
+{
+	wp_enqueue_style('multi-block-frontend-js', plugin_dir_url(__FILE__) . 'build/style-multi-block-editor.css',);
+	wp_enqueue_script('multi-block-frontend-css', plugin_dir_url(__FILE__) . 'build/multi-block-frontend.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'multiblock_enqueue_frontend_assets');
