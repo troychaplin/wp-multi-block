1e03f11d82a30efe12626313d45e975cf0da4d56
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-30 18:30:35 -0400

    updated styles and text domain

--- build/blocks/block-four/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e4d79951cd2ff8c4bcdb');
---
+++ build/multi-block-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '38b54ad2d6773e2a4def');
