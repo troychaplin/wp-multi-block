f545fb12b6e0007c9681245043db2e2746328c98
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-30 17:53:05 -0400

    add dynamic block, update register block type, update package, ran build

--- wp-multi-block.php
+++ wp-multi-block.php
@@ -25,5 +25,6 @@ function create_block_wp_multi_block_block_init()
 {
 	register_block_type(__DIR__ . '/build/blocks/block-one');
 	register_block_type(__DIR__ . '/build/blocks/block-two');
+	register_block_type(__DIR__ . '/build/blocks/block-three');
 }
 add_action('init', 'create_block_wp_multi_block_block_init');
