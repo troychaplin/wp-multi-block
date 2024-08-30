b2df9a3af15598acf03ce75bbcbf2f25e2fe1e6e
Troy Chaplin <troy.chaplin@gmail.com>
2024-08-30 17:49:31 -0400

    cleaned up src, added new block

--- src/save.js
+++
@@ -1,9 +0,0 @@
-import { useBlockProps } from '@wordpress/block-editor';
-
-export default function save() {
-	return (
-		<p {...useBlockProps.save()}>
-			{'Wp Multi Block – hello from the saved content!'}
-		</p>
-	);
-}
